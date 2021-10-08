67c276f7ee856621211bae6b680a343180db3e36
nikodimos <nikodimos1111endeshaw@gmail.com>
2021-10-08 16:04:20 +0300

    update main

--- main.h
+++ main.h
@@ -1,29 +1,39 @@
-#ifndef MAINH
-#define MAINH
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+/**
+ * struct mystr - pointer to function with corresponding letter
+ * @letter: specifier
+ * @func: print argument
+ */
+typedef struct mystr
+{
+	char *letter;
+	int (*func)(va_list);
+} mystr;
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_rev(va_list r);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rot13(va_list R);
-/**
-  * struct code_format - Struct format
-  *
-  * @sc: The specifiers
-  * @f: The function associated
-  */
-typedef struct code_format
-{
-char *sc;
-int (*f)(va_list);
-}
-code_f;
+int(*getspecifier(const char *format, int i))(va_list);
+/** fn in file : fn_char.c */
+int print_c(va_list c);
+int print_s(va_list s);
+int print_S(va_list S);
+int print_rs(va_list rs);
+int print_rot(va_list ro);
+/** fn in file : fn_numbers.c */
+int _print_i(va_list vi);
+int _print_b(va_list b);
+/** fn in file : fn_uoxX.c */
+int _print_u(va_list);
+int _print_o(va_list);
+int _print_x(va_list);
+int _print_X(va_list);
+int _print_p(va_list);
+
+int strange(const char *format, int i);
+int _plus_i(va_list);
+int _space_i(va_list);
+int _diez_o(va_list);
+int _diez_x(va_list);
+int _diez_X(va_list);
 #endif
