a42e79e321c0c6cc3eae33ba0562730b1e734fcc
nikodimos <nikodimos1111endeshaw@gmail.com>
2021-10-08 15:57:43 +0300

    update printf

--- _printf.c
+++ _printf.c
@@ -1,76 +1,48 @@
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 /**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
-{
-unsigned int i = 0;
-code_f find_f[] = {
-{"c", print_char},
-{"s", print_string},
-{"i", print_int},
-{"d", print_dec},
-{"r", print_rev},
-{"b", print_bin},
-{"u", print_unsig},
-{"o", print_octal},
-{"x", print_x},
-{"X", print_X},
-{"R", print_rot13},
-{NULL, NULL}
-};
-while (find_f[i].sc)
-{
-if (find_f[i].sc[0] == (*format))
-return (find_f[i].f);
-i++;
-}
-return (NULL);
-}
-/**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
+ * _printf - prints anything
+ * @format: pointer to string that contains specifiers
+ * Return: number of characters printed
+ **/
 int _printf(const char *format, ...)
 {
-va_list ap;
-int (*f)(va_list);
-unsigned int i = 0, cprint = 0;
-if (format == NULL)
-return (-1);
-va_start(ap, format);
-while (format[i])
-{
-while (format[i] != '%' && format[i])
-{
-_putchar(format[i]);
-cprint++;
-i++;
-}
-if (format[i] == '\0')
-return (cprint);
-f = find_function(&format[i + 1]);
-if (f != NULL)
-{
-cprint += f(ap);
-i += 2;
-continue;
-}
-if (!format[i + 1])
-return (-1);
-_putchar(format[i]);
-cprint++;
-if (format[i + 1] == '%')
-i += 2;
-else
-i++;
-}
-va_end(ap);
-return (cprint);
+	unsigned int count = 0, i = 0;
+	int (*f)(va_list);
+	va_list list;
+
+	if (format == '\0')
+		return (-1);
+	va_start(list, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		else if (format[i] == '\0')
+			return (count);
+		else if (format[i] == '%')
+		{
+			f = getspecifier(format, i + 1);
+			i += 1;
+			if (f == NULL)
+			{
+				count += strange(format, i);
+			}
+			else
+			{
+				count = count + f(list);
+				if (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+					i++;
+			}
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
 }
